be5417e2db2d445b2e985e8f8f6b218d42c045b7
Maxwell <star32975@gmail.com>
2021-03-06 19:30:27 +0200

    +Admin panel, +Rework design(sidebar!)

--- MyCompany/Controllers/HomeController.cs
+++ MyCompany/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyCompany.Domain;
 
 namespace MyCompany.Controllers
 {
     public class HomeController : Controller
     {
+        private DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
---
+++ MyCompany/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+
+namespace MyCompany.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult) View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
